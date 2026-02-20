5226f98c40e3f3a8ad4bb814f12412d9b8a40bdc
Joseph Sierra <joseph.sgtz@gmail.com>
2026-02-19 22:57:22 -0600

    add users models and covergae config

---
+++ apps/users/admin.py
@@ -0,0 +1,64 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField, UserChangeForm
+
+from apps.users.models import User
+
+
+class CustomUserChangeForm(UserChangeForm):
+    """
+    Custom user change form class
+    """
+
+    password = ReadOnlyPasswordHashField(
+        label="Password",
+        help_text=(
+            "Raw passwords are not stored, so there is no way to see "
+            "this user's password, but you can change the password "
+            'using <a href="{}">this form</a>.'
+        ),
+    )
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    Custom UserAdmin class
+    """
+
+    form = CustomUserChangeForm
+    readonly_fields = ("uuid", "date_joined", "last_login")
+    fieldsets = (
+        ("Login", {"fields": ("email", "password")}),
+        (
+            "Data",
+            {
+                "fields": (
+                    "uuid",
+                    "name",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")},
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "is_staff", "date_joined", "last_login")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("email", "name")
+    ordering = ("date_joined",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
---
+++ apps/users/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+
+from app.models import UUIDModel
+
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin, UUIDModel):
+    """
+    User Model
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=64, null=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now, editable=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
