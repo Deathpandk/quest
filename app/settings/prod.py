05abd7421f46d5f9000d038c95a0dde6f1279fba
Joseph Sierra <joseph.sgtz@gmail.com>
2026-03-11 15:31:30 -0600

    add dj db

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -5,7 +5,7 @@ from .base import *
 DEBUG = False
 
 DB_STRING = env.str("DATABASE_URL", default=None)
-DATABASES = {"default": dj_database_url.parse(DB_STRING)}
+DATABASES = {"default": env.db(DB_STRING)}
 
 STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "/static/"
