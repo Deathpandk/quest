d2e61a39580989497b5e6d7552cfad0b872501e3
Joseph Sierra <joseph.sgtz@gmail.com>
2026-03-11 14:25:29 -0600

    add prod statics

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -6,3 +6,6 @@ DEBUG = False
 
 DB_STRING = env.str("DATABASE_URL", default=None)
 DATABASES = {"default": dj_database_url.parse(DB_STRING)}
+
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "/static/"
