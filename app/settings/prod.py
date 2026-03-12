b8a07cfa30d3098e0d66b66b5111809b103b82f8
Joseph Sierra <joseph.sgtz@gmail.com>
2026-03-12 11:36:16 -0600

    add allowed hosts

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -4,6 +4,7 @@ DEBUG = True
 
 SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 DATABASES = {"default": env.db("DATABASE_URL")}
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
 STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "/static/"
