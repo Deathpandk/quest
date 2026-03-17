3e02d708476c0dac38abb70931c2be73a8352179
Joseph Sierra <joseph.sgtz@gmail.com>
2026-03-16 23:34:07 -0600

    delete duplicates

--- apps/inventory/management/commands/check_change_duplicates.py
+++ apps/inventory/management/commands/check_change_duplicates.py
@@ -6,7 +6,6 @@ from apps.inventory.models import Inventory, InventoryChange
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        items_count = 0
         print("=" * 20)
         for item in Inventory.objects.all():
             changes = InventoryChange.objects.filter(product_variation=item.product_variation)
@@ -14,22 +13,4 @@ class Command(BaseCommand):
             if len(changes) == 1:
                 continue
 
-            reference = changes.first().change
-            abort = False
-            for c in changes:
-                if c.change != reference:
-                    abort = True
-
-            if abort:
-                continue
-
-            items_count += 1
-            if items_count > 100:
-                print("changed 100 items")
-                break
-
-            for index, c in enumerate(changes):
-                if index != 0:
-                    c.delete()
-
-            print(item.product_variation, reference)
+            print(item.product_variation, "CAMBIOS", len(changes))
