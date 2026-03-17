474a377df86419fd4052743ecec7bb6447b24c97
Joseph Sierra <joseph.sgtz@gmail.com>
2026-03-17 00:01:39 -0600

    Add jwt endpoints

--- app/urls.py
+++ app/urls.py
@@ -3,10 +3,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    path(
+        "api/v1/auth/",
+        TokenObtainPairView.as_view(),
+        name="auth_get_token",
+    ),
+    path("api/v1/auth/refresh/", TokenRefreshView.as_view(), name="auth_refresh_token"),
 ]
 
 if settings.ENV != "test":
